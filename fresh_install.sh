75622b0225710e05814de454ede8d5acbc58ca03
Kevin Seidel <kevin@Kevins-Mac-mini.local>
2015-04-16 14:27:47 +0200

    Added nmap

--- fresh_install.sh
+++ fresh_install.sh
@@ -3,7 +3,7 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew update
-brew install android-sdk bash brew-cask carthage curl ettercap go grep mariadb openssh node openssl python tmux vim wget
+brew install android-sdk bash brew-cask carthage curl ettercap go grep mariadb openssh node nmap openssl python tmux vim wget
 brew cask install adium alfred android-studio appcleaner atext beamer brackets cocoapacketanalyzer dropbox firefox iterm2 libreoffice limechat macdown sequel-pro slack smoothmouse spotify teamviewer textmate thunderbird transmission virtualbox vlc
 cd ~/Documents
 git clone https://github.com/moogle19/dotfiles.git
