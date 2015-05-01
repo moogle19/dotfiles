85ef436b083084ff9a159dd052adad7dcf013a25
Kevin <moogle@Kevins-MacBook-Pro.local>
2015-05-01 21:04:48 +0200

    Fixed brew installs and added installation of nsf/gocode for vim code completion

--- fresh_install.sh
+++ fresh_install.sh
@@ -3,12 +3,13 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew update
-brew install android-sdk bash brew-cask carthage curl ettercap go grep mariadb openssh node nmap openssl python tmux vim wget
+brew install android-sdk bash caskroom/cask/brew-cask carthage curl ettercap go homebrew/dupes/grep mariadb openssh node nmap openssl python tmux vim wget
 brew cask install adium alfred android-studio appcleaner atext beamer brackets cocoapacketanalyzer dropbox firefox iterm2 libreoffice limechat macdown sequel-pro slack smoothmouse spotify teamviewer textmate thunderbird transmission virtualbox vlc
 cd ~/Documents
 git clone https://github.com/moogle19/dotfiles.git
 cd dotfiles
 git submodule init && git submodule update
+go get -u github.com/nsf/gocode
 
 
 ln -s ${PWD}/.oh-my-zsh ~/.oh-my-zsh
