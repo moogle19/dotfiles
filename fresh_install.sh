f63d6cf004630b66f7ebcba8aef6cc119259fa0b
Kevin Seidel <git@kseidel.org>
2015-10-07 21:34:13 +0200

    Add psequel cask

--- fresh_install.sh
+++ fresh_install.sh
@@ -4,7 +4,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew doctor
 brew update
 brew install bash caskroom/cask/brew-cask cabal-install carthage curl erlang ettercap exercism git gnupg2 go homebrew/dupes/grep haskell-stack htop-osx hub libressl mysql mongodb netcat nmap node openssh openssl perl postgresql pstree pv python rebar ruby rust sqlite tmux tree unrar vim wget zsh
-brew cask install alfred appcleaner atext atom beamer brackets chitchat cocoapacketanalyzer controlplane dash disk-inventory-x firefox google-chrome iterm2 java keepassx keepingyouawake macdown sequel-pro slack smoothmouse spectacle spotify teamviewer textmate the-unarchiver thunderbird transmission tunnelblick virtualbox vlc
+brew cask install alfred appcleaner atext atom beamer brackets chitchat cocoapacketanalyzer controlplane dash disk-inventory-x firefox google-chrome iterm2 java keepassx keepingyouawake macdown psequel sequel-pro slack smoothmouse spectacle spotify teamviewer textmate the-unarchiver thunderbird transmission tunnelblick virtualbox vlc
 cd ~/Documents
 git clone https://github.com/moogle19/dotfiles.git
 cd dotfiles
