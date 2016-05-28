f74c77911f501311c6e163e3c32d522266a85a1a
moogle19 <mail@kseidel.org>
2016-05-29 00:09:12 +0200

    Update brew command and switch hermit to mononoki

--- fresh_install.sh
+++ fresh_install.sh
@@ -3,14 +3,78 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew update
-brew install bash caskroom/cask/brew-cask cabal-install carthage curl erlang ettercap exercism git gnupg2 go homebrew/dupes/grep haskell-stack htop-osx hub libressl mysql mongodb netcat nmap node openssh openssl perl postgresql pstree pv python rebar ruby rust sqlite tmux tree unrar vim wget zsh
-brew cask install alfred appcleaner atext atom beamer brackets chitchat cocoapacketanalyzer controlplane dash disk-inventory-x firefox google-chrome iterm2 itsycal java keepassx keepingyouawake macdown psequel sequel-pro slack smoothmouse spectacle spotify teamviewer textmate the-unarchiver thunderbird transmission tunnelblick virtualbox vlc
+brew install \ 
+	bash \ 
+	caskroom/cask/brew-cask \
+	carthage \
+	curl \
+	erlang \
+	ettercap \
+	git \
+	gnupg2 \
+	go \
+	homebrew/dupes/grep \
+	htop-osx \
+	hub \
+	libressl \
+	mysql \
+	mongodb \
+	netcat \
+	nmap \
+	node \
+	openssh \
+	openssl \
+	perl \
+	postgresql \
+	pstree \
+	pv \
+	python \
+	rebar \
+	ruby \
+	sqlite \
+	tmux \
+	tree \
+	unrar \
+	vim \
+	wget \
+	zsh
+
+brew cask install \
+	alfred \
+	appcleaner \
+	atext \
+	beamer \
+	cocoapacketanalyzer \
+	controlplane \
+	dash \
+	disk-inventory-x \
+	firefox \
+	google-chrome \
+	iterm2 \
+	itsycal \
+	keepassx \
+	keepingyouawake \
+	macdown \
+	psequel \
+	sequel-pro \
+	slack \
+	smoothmouse \
+	spotify \
+	teamviewer \
+	textmate \
+	the-unarchiver \
+	thunderbird \
+	transmission \
+	tunnelblick \
+	virtualbox \
+	vlc
+
 cd ~/Documents
 git clone https://github.com/moogle19/dotfiles.git
 cd dotfiles
 git submodule init && git submodule update
 go get -u github.com/nsf/gocode
-sh install_hermit.sh
+sh install_mononoki.sh
 
 
 ln -s ${PWD}/.oh-my-zsh ~/.oh-my-zsh
--- install_hermit.sh
+++
@@ -1,8 +0,0 @@
-cd ~/Downloads
-wget https://pcaro.es/d/otf-hermit-1.21.tar.gz
-mkdir hermit
-tar xvf otf-hermit-1.21.tar.gz -C hermit
-scp hermit/Hermit* ~/Library/Fonts
-rm -rf hermit
-rm otf-hermit-1.21.tar.gz
-
