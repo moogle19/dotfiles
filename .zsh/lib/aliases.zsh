3a77e9cbc2ed7792f7e7e7625db30e201073ff26
moogle <mail@kseidel.org>
2019-12-18 11:12:23 +0100

    Add aliases

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -4,3 +4,10 @@ alias vi="nvim"
 alias todo="vim ~/Documents/.todo"
 
 alias pq1="pgcli -h 192.168.136.98 -d Stammdaten -u postgres"
+
+alias li="open https://lichess.org"
+
+function pando() {
+  filename="$(basename $1 .md)"
+  pandoc $1 -f markdown --template eisvogel --listings -o $filename.pdf
+}
